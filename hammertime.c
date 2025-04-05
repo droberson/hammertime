d30d16b4cf9831f641f8824ac0e394790ce857f5
daniel <daniel@planethacker.net>
2025-04-05 08:17:33 -0700

    whitespace

--- hammertime.c
+++ hammertime.c
@@ -6,7 +6,7 @@
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Daniel Roberson");
-MODULE_DESCRIPTION("Invoke cleanup_module from user-supplied address slide out into the darkness.");
+MODULE_DESCRIPTION("Invoke cleanup_module from user-supplied address slide out.");
 MODULE_VERSION("0.1");
 
 static unsigned long cleanup_fn = 0;
@@ -15,25 +15,24 @@ MODULE_PARM_DESC(cleanup_fn, "Address of the target cleanup_module() function");
 
 static int __init cool_removal_init(void)
 {
-    if (!cleanup_fn) {
-        pr_err("No cleanup_module() address provided.\n");
-        return -EINVAL;
-    }
+	if (!cleanup_fn) {
+		pr_err("No cleanup_module() address provided.\n");
+		return -EINVAL;
+	}
 
-    pr_info("Calling cleanup_module() at 0x%lx\n", cleanup_fn);
+	pr_info("Calling cleanup_module() at 0x%lx\n", cleanup_fn);
 
-    // cast and call it
-    ((void (*)(void))cleanup_fn)();
+	// cast and call it
+	((void (*)(void))cleanup_fn)();
 
-    // fail init so we unload automatically
-    return -ECANCELED;
+	// fail init so we unload automatically
+	return -ECANCELED;
 }
 
 static void __exit cool_removal_exit(void)
 {
-    pr_info("cool_removal_module exiting cleanly.\n");
+	pr_info("cool_removal_module exiting cleanly.\n");
 }
 
 module_init(cool_removal_init);
 module_exit(cool_removal_exit);
-
