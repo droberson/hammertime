c306f963c88681f54e3522a9be6a649535e5aea6
daniel <daniel@planethacker.net>
2025-04-05 09:13:44 -0700

    style and naming cleanup

--- hammertime.c
+++ hammertime.c
@@ -7,13 +7,13 @@
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Daniel Roberson");
 MODULE_DESCRIPTION("Invoke cleanup_module from user-supplied address slide out.");
-MODULE_VERSION("0.1");
+MODULE_VERSION("0.2");
 
 static unsigned long cleanup_fn = 0;
 module_param(cleanup_fn, ulong, 0000);
 MODULE_PARM_DESC(cleanup_fn, "Address of the target cleanup_module() function");
 
-static int __init cool_removal_init(void)
+static int __init hammertime_init(void)
 {
 	if (!cleanup_fn) {
 		pr_err("No cleanup_module() address provided.\n");
@@ -21,18 +21,15 @@ static int __init cool_removal_init(void)
 	}
 
 	pr_info("Calling cleanup_module() at 0x%lx\n", cleanup_fn);
-
-	// cast and call it
 	((void (*)(void))cleanup_fn)();
 
-	// fail init so we unload automatically
-	return -ECANCELED;
+	return -ECANCELED; /* return -ECANCELED so module does not remain loaded */
 }
 
-static void __exit cool_removal_exit(void)
+static void __exit hammertime_exit(void)
 {
-	pr_info("cool_removal_module exiting cleanly.\n");
+	pr_info("hammertime exiting cleanly.\n");
 }
 
-module_init(cool_removal_init);
-module_exit(cool_removal_exit);
+module_init(hammertime_init);
+module_exit(hammertime_exit);
